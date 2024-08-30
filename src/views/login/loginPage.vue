a4b831ea763ccc2f0c061409af10f6f7cfb2ad87
许鸿志 <847118663@qq.com>
2024-08-30 15:40:50 +0800

    登陆界面开发

--- src/views/login/loginPage.vue
+++ src/views/login/loginPage.vue
@@ -1,29 +1,59 @@
 <template>
-  <div class="login">
-    <h1>Login</h1>
-    <form @submit.prevent="loginHandler">
-      <label for="username">Username:</label>
-      <input type="text" id="username" v-model="loginParam.username" required>
-      <label for="password">Password:</label>
-      <input type="password" id="password" v-model="loginParam.password" required>
-      <button type="submit">Login</button>
-    </form>
-  </div>
+    <div  class="login">
+      <div class="loginBgImg">
+        <el-image style="width: 100%; height: auto" :src="bgImg" fit="contain"></el-image>
+      </div>
+      <div class="content">
+        <div class="subTitle">欢迎登陆~~</div>
+        ocpx-web
+        <el-tabs v-model="activeName" class="demo-tabs">
+          <!--   账号登陆-->
+          <el-tab-pane label="账号登陆" name="first">
+            <el-form :model="userNameLoginParam" label-width="120px">
+              <el-form-item label="请输入账号:">
+                <el-input v-model="userNameLoginParam.username"/>
+              </el-form-item>
+              <el-form-item label="请输入密码:" prop="pass">
+                <el-input v-model="userNameLoginParam.password" type="password" autocomplete="off"/>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="loginHandler">登陆</el-button>
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+
+          <!--   手机号登陆-->
+          <el-tab-pane label="手机号登陆" name="second">
+            <el-form :model="phoneLoginParam" label-width="120px">
+              <el-form-item label="请输入手机号">
+                <el-input v-model="phoneLoginParam.phoneNumber"/>
+              </el-form-item>
+              <el-form-item label="验证码" prop="pass">
+                <el-input v-model="phoneLoginParam.code"/>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="loginHandler">登陆</el-button>
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </div>
 </template>
 
 <script setup>
 import store from '../../store';
-import {login} from "@/api/api";
-import {reactive} from 'vue';
-import {ElMessage} from 'element-plus';
+import { login } from "@/api/api";
+import { reactive, ref } from 'vue';
+import { ElMessage } from 'element-plus';
+import { useRouter } from 'vue-router';
+import bgImg from '@/assets/image/login/bgImg.png';
 
-const loginParam = reactive({
-  username: '',
-  password: ''
-});
-import {useRouter} from 'vue-router';
-
+const activeName = ref('first');
+const userNameLoginParam = reactive({ username: '', password: '' });
+const phoneLoginParam = reactive({ phoneNumber: '', code: '' });
 const router = useRouter();
+
 
 // const loginHandler = async () => {
 //   try {
@@ -38,7 +68,7 @@ const router = useRouter();
 // };
 
 const loginHandler = () => {
-  login(loginParam).then(response => {
+  login(userNameLoginParam).then(response => {
     console.log('Request successful', response.data);
     store.commit('USER_TOKEN', response.data);
     localStorage.setItem('CLOUD-AFTER-CLASS-TOKEN', response.data);
@@ -52,33 +82,25 @@ const loginHandler = () => {
 </script>
 
 <style scoped>
+
 .login {
-  max-width: 300px;
-  margin: 0 auto;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-
-form {
   display: flex;
+  max-width: 800px; /* 根据需要调整最大宽度 */
+  margin: 0 auto;   /* 使容器水平居中 */
+  height: 100%;   /* 撑满父容器 */
+}
+
+.loginBgImg, .content {
+  flex: 1;              /* 确保背景图片和表单各占50%宽度 */
+  display: flex;        /* 使子元素居中 */
+  justify-content: center; /* 水平居中 */
+  align-items: center;    /* 垂直居中 */
+}
+
+.content {
   flex-direction: column;
-  gap: 10px;
-}
-
-label {
-  font-weight: bold;
-}
-
-input {
-  padding: 5px;
-}
-
-button {
-  background-color: #007bff;
-  color: #fff;
-  padding: 10px;
-  border: none;
-  cursor: pointer;
+  padding: 20px;
+  background-color: white; /* 可根据需求调整背景颜色 */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 </style>
